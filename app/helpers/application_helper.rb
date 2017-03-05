5aa0b2e0ffef64a352a433091038e32b6260f5ff
DaniVSainz <danivsainz@gmail.com>
2017-03-05 12:16:26 -0500

    Add devise mailboxer bootsass, made partial _nav

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,10 @@
+module ApplicationHelper
+  def flash_class(level)
+    case level.to_sym
+      when :notice then "alert alert-success"
+      when :info then "alert alert-info"
+      when :alert then "alert alert-danger"
+      when :warning then "alert alert-warning"
+    end
+  end
+end
