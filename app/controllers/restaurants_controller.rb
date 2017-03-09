256534334bd0fa887036d865d01db03160f23c81
DaniVSainz <danivsainz@gmail.com>
2017-03-08 22:49:15 -0500

    Update Restaurant controller with ENV[KEYS]

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,8 +6,8 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
     @search = params[:term]
-    @clientid = "T2RKEKNJZAVTB133DXTYNSNYSAHGFJ0BWFRANKUGXRKAPJIG"
-    @client_secret = "XHEOYT40SRPC0BVNPS1WSLCIESFCSAKE40YZQ0P1D4EEX41X"
+    @clientid = ENV['CLIENT_ID']
+    @client_secret = ENV['CLIENT_SECRET']
     @todays_date = 20170307
     @userless_request = "https://api.foursquare.com/v2/venues/explore?client_id=#{@clientid}&client_secret=#{@client_secret}&ll=25.803076,-80.204268&v=#{@todays_date}&query=#{@search}&venuePhotos=1"
     @response = HTTParty.get @userless_request
