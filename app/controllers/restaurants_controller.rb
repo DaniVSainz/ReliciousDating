302f4267724f917cf31c34dcc7dd161c3586b078
DaniVSainz <danivsainz@gmail.com>
2017-03-08 23:27:39 -0500

    Add user authentication to restaurant's page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_restaurant, only: [ :edit, :update, :destroy] #:show,
 
   # GET /restaurants
