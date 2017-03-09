e310b2770a431c447f212dea131d8af1344ab65b
DaniVSainz <danivsainz@gmail.com>
2017-03-08 22:24:19 -0500

    import code into restaurants/controller INSIDE(def index) method TO Get  Api request code/Search function FOR index.html.erb

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,12 @@ class RestaurantsController < ApplicationController
   # GET /restaurants.json
   def index
     @restaurants = Restaurant.all
+    @search = params[:term]
+    @clientid = "T2RKEKNJZAVTB133DXTYNSNYSAHGFJ0BWFRANKUGXRKAPJIG"
+    @client_secret = "XHEOYT40SRPC0BVNPS1WSLCIESFCSAKE40YZQ0P1D4EEX41X"
+    @todays_date = 20170307
+    @userless_request = "https://api.foursquare.com/v2/venues/explore?client_id=#{@clientid}&client_secret=#{@client_secret}&ll=25.803076,-80.204268&v=#{@todays_date}&query=#{@search}&venuePhotos=1"
+    @response = HTTParty.get @userless_request
   end
 
   # GET /restaurants/1
