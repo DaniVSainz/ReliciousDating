d59b9a742a33b8a5b7e71deb534e847211e8a8a1
DaniVSainz <danivsainz@gmail.com>
2017-03-11 14:12:14 -0500

    Add avatar to new/edit devise registrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308041242) do
+ActiveRecord::Schema.define(version: 20170311190908) do
 
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -99,6 +99,19 @@ ActiveRecord::Schema.define(version: 20170308041242) do
     t.datetime "updated_at",    null: false
   end
 
+  create_table "pictures", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "body"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
     t.string   "gender_pref"
@@ -134,6 +147,10 @@ ActiveRecord::Schema.define(version: 20170308041242) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
