90cc654c16dbde23eb4771e5cff37cda1d886f29
DaniVSainz <danivsainz@gmail.com>
2017-03-11 16:59:03 -0500

    Add AWS s3 , it's currently uploading having problems retreiving.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
 end
+#Amazon webservices
+gem 'aws-sdk'
+#Handles image attachments
 gem 'paperclip'
 # Hiding our api keys with a .env file wtih dotenv-rails
 gem 'dotenv-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   :recoverable, :rememberable, :trackable , :omniauthable
   # Paperclip Devise
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, content_type: /\Aimage/
 
 
   has_many :identities
