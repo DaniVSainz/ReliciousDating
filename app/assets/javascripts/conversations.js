24649de133bc50bb48638ba574fdac7ee7f139e4
DaniVSainz <danivsainz@gmail.com>
2017-03-13 15:58:48 -0400

    Commented out js for conversations select

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,19 +1,19 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-var ready;
+// var ready;
+//
+// ready = function(){
+//     // enable chosen js
+//     $('.chosen-select').chosen({
+//         no_results_text: 'No results matched'
+//     });
+// }
 
-ready = function(){
-    // enable chosen js
-    $('.chosen-select').chosen({
-        no_results_text: 'No results matched'
-    });
-}
-
-$( document ).ready(function() {
+$( document ).ready(function(){
   $('.chosen-select').chosen({
       no_results_text: 'No results matched'
   });
 });
 
-$(document).on("page:load",ready);
+// $(document).on("page:load",ready);
