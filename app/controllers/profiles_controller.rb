1f1d04d54de7130aa11bca3a4b9a8f9f17c891b3
DaniVSainz <danivsainz@gmail.com>
2017-03-12 22:51:40 -0400

    Add reroute on profile creation to index show// add placeholder image to assets

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -29,7 +29,7 @@ class ProfilesController < ApplicationController
 
     respond_to do |format|
       if @profile.save
-        format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
+        format.html { redirect_to profiles_path, notice: 'Profile was successfully created.' }
         format.json { render :index, status: :created, location: @profile }
       else
         format.html { render :new }
