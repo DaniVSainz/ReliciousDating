462c64634158d7b9af6530bb3410088f0c03622e
DaniVSainz <danivsainz@gmail.com>
2017-03-12 22:45:07 -0400

    Add user images to sent and inbox

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -30,7 +30,7 @@ class ProfilesController < ApplicationController
     respond_to do |format|
       if @profile.save
         format.html { redirect_to @profile, notice: 'Profile was successfully created.' }
-        format.json { render :show, status: :created, location: @profile }
+        format.json { render :index, status: :created, location: @profile }
       else
         format.html { render :new }
         format.json { render json: @profile.errors, status: :unprocessable_entity }
